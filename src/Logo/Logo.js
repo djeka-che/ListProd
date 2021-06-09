462258eb6f176a9aa6ef6accebf01ac923e090af
djeka-che <djeka-che@mail.ru>
2021-06-09 22:24:58 +0300

    'ListProd'

---
+++ src/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logo(props) {
+    return (
+        <div>
+            <img src='my-img/full-word.svg' alt='Counter'/>
+        </div>
+    );
+};
+
+export default Logo;
